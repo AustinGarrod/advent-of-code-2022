6bb11c366ea1cf7d9d4cd50a482f10b8ce7492c1
Austin Garrod <austin@epiloguewills.com>
2022-12-01 11:46:26 -0500

    day 1 - part 2

--- day01/index.ts
+++ day01/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import readline from 'readline'
 
-const getLongBoi = async () => {
+const part1 = async () => {
   const fileStream = fs.createReadStream('./day01/input.txt')
   const fileLines = readline.createInterface({
     input: fileStream,
@@ -24,4 +24,30 @@ const getLongBoi = async () => {
   console.log(`Largest calories held: ${largestTotal}`)
 }
 
-getLongBoi()
+const part2 = async () => {
+  const fileStream = fs.createReadStream('./day01/input.txt')
+  const fileLines = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+
+  let accumulator: number[] = []
+  let totals: number[] = []
+
+  for await (const line of fileLines) {
+    if (!line) {
+      const total = accumulator.reduce((a, b) => a + b, 0)
+      totals.push(total)
+      accumulator = []
+    } else {
+      accumulator.push(parseInt(line))
+    }
+  }
+
+  const topThreeTotal = totals.sort((a, b) => b - a).slice(0, 3).reduce((a, b) => a + b, 0)
+
+  console.log(`Total of top 3 calories held: ${topThreeTotal}`)
+}
+
+part1()
+part2()
