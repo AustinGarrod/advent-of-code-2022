ee0da66b3f87f1272aaaa16d24119a79eff160b7
Austin Garrod <austin@epiloguewills.com>
2022-12-03 17:47:54 -0500

    part 2

--- day03/index.ts
+++ day03/index.ts
@@ -86,7 +86,51 @@ const part1 = async () => {
     }
   }
 
-  console.log(total)
+  console.log(`Your total for part 1 is: ${total}`)
+}
+
+const part2 = async () => {
+  const fileStream = fs.createReadStream('./day03/input.txt')
+  const fileLines = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  })
+  let total = 0
+  let groups: Array<string[]> = []
+
+  for await (const line of fileLines) {
+    if (groups.length === 0 || groups[groups.length - 1].length === 3) {
+      groups.push([line])
+    } else {
+      groups[groups.length - 1].push(line)
+    }
+  }
+
+  for (let groupPosition = 0; groupPosition < groups.length; groupPosition++) {
+    const group = groups[groupPosition]
+    const firstContainer = group[0].split('')
+    const secondContainer = group[1].split('')
+    const thirdContainer = group[2].split('')
+
+    for (
+      let firstContainerPosition = 0;
+      firstContainerPosition < firstContainer.length;
+      firstContainerPosition++
+    ) {
+      const firstItem = firstContainer[firstContainerPosition]
+
+      if (
+        secondContainer.some((secondItem) => secondItem === firstItem) &&
+        thirdContainer.some((thirdItem) => thirdItem === firstItem)
+      ) {
+        total += getPoints(firstItem)
+        firstContainerPosition = firstContainer.length
+      }
+    }
+  }
+
+  console.log(`Your total for part 2 is: ${total}`)
 }
 
 part1()
+part2()
